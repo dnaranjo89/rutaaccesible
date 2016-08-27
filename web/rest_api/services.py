fb3856681cf72c0eea622cae9e4dd40567e11190
David Naranjo <dnaranjo89@gmail.com>
2016-08-27 04:20:12 +0200

    Add parameters to urls

--- web/rest_api/services.py
+++ web/rest_api/services.py
@@ -1,4 +1,5 @@
 from flask import jsonify
+from flask.ext.restful import reqparse
 from flask_restful import Resource, Api
 
 from web import webapp
@@ -6,9 +7,28 @@ from web.models import ParkingSlot
 
 api = Api(webapp)
 
-class ParkingSlotResource(Resource):
+
+class ParkingSlotListAPI(Resource):
     def get(self):
         parking_slots = ParkingSlot.query.all()
         return jsonify([parking_slot.serialize for parking_slot in parking_slots])
 
-api.add_resource(ParkingSlotResource, '/test_api')
+
+class ParkingSlotAPI(Resource):
+    def get(self, id):
+        return ParkingSlot.query.filter_by(id=id).first().serialize
+
+
+class ClosestParkingSlotAPI(Resource):
+    def get(self):
+        parser = reqparse.RequestParser()
+        lat = parser.add_argument('lat', type=float)
+        lng = parser.add_argument('lng', type=float)
+        eee = parser.parse_args()
+
+        return ParkingSlot.query.filter_by(id=id).first().serialize
+
+
+# api.add_resource(ParkingSlotListAPI, '/api/v1.0/parking_slot')
+api.add_resource(ParkingSlotAPI, '/api/v1.0/parking_slot/<int:id>')
+api.add_resource(ClosestParkingSlotAPI, '/api/v1.0/parking_slot')
