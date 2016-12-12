0f95bbd877a326375d7637758b6207b6f077c787
David Naranjo <dnaranjo89@gmail.com>
2016-12-12 01:40:36 +0100

    remove unused code

--- web/database.py
+++ web/database.py
@@ -6,20 +6,3 @@ def get_closest_parking_slots(location):
     db.engine.execute('select * from parking_slot')
     print(parking_slots)
 
-
-'''
-http://www.plumislandmedia.net/mysql/haversine-mysql-nearest-loc/
-
-SELECT id, pos_lat, pos_long,
-      111.045* DEGREES(ACOS(COS(RADIANS(latpoint))
-                 * COS(RADIANS(pos_lat))
-                 * COS(RADIANS(longpoint) - RADIANS(pos_long))
-                 + SIN(RADIANS(latpoint))
-                 * SIN(RADIANS(pos_lat)))) AS distance_in_km
- FROM parking_slot
- JOIN (
-     SELECT  39.472  AS latpoint,  -6.40567 AS longpoint
-   ) AS p ON 1=1
- ORDER BY distance_in_km
- LIMIT 15;
-'''
